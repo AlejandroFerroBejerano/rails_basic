aed06346f696128c518a5cb868ce7b5419642c89
dalarenal <arenal.diego@gmail.com>
2014-05-10 02:37:38 +0200

    Added foreman config. Added pages views and controller structure.

---
+++ config/unicorn.production.rb
@@ -0,0 +1,36 @@
+# config/unicorn.rb
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 60
+preload_app true
+
+APP_PATH = ENV['HOME']+"/frontend"
+working_directory APP_PATH
+
+listen ENV["HOME"] + "/shared/socket/unicorn.sock", :backlog => 64
+pid ENV["HOME"] + "/shared/pid/unicorn.pid"
+stderr_path ENV['HOME'] + "/shared/log/unicorn_err.log" if ENV["RAILS_ENV"] == "production"
+stdout_path ENV['HOME'] + "/shared/log/unicorn_out.log" if ENV["RAILS_ENV"] == "production"
+
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+  defined?(Analytics) and
+    Analytics.init(secret: ENV['SEGMENTIO_API_SECRET'])
+end
